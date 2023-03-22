eb18eea13445adcbf65310c166c61704cd791c55
redasys <bob.egan@alliedpayment.com>
2023-03-22 16:28:20 -0400

    error handling

--- index.js
+++ index.js
@@ -2,44 +2,46 @@ const core = require('@actions/core')
 const replaceInFiles = require('replace-in-files');
 
 const options = {
-    files: ()=>{
-      return core.getInput('path-to-watch');
-    },
-    from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g,
-    to: '${ApiEndpoint}'
+  files: () => {
+    return core.getInput('path-to-watch');
+  },
+  from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g,
+  to: '${ApiEndpoint}'
 }
 
-function run(){
+function run() {
+  try {
     core.notice(core.getInput('path-to-watch'));
     replaceInFiles(options)
-  .then(({ changedFiles, countOfMatchesByPaths }) => {
-    core.notice('Modified files:', changedFiles);
-    core.setOutput("changedFiles", changedFiles)
-    core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
-    core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
-    core.notice('options', options);
-    core.setOutput("options", options)
-    
-  })
-  .catch(error => {
-    core.notice('Error occurred:', error);
-  });
+      .then(({ changedFiles, countOfMatchesByPaths }) => {
+        core.notice('Modified files:', changedFiles);
+        core.setOutput("changedFiles", changedFiles)
+        core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
+        core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
+        core.notice('options', options);
+        core.setOutput("options", options)
 
-  options.from = /"Stage": "Test"/g;
-  options.to = '"Stage": "${stage}"';
+      })
+      .catch(error => {
+        core.notice(`Error occurred: ${error}${error.toString()}`);
+      });
 
-  replaceInFiles(options)
-  .then(({ changedFiles, countOfMatchesByPaths }) => {
-    core.notice('Modified files:', changedFiles);
-    core.setOutput("changedFiles", changedFiles)
-    core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
-    core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
-    core.notice('options', options);
-    core.setOutput("options", options)
-  })
-  .catch(error => {
-    core.notice('Error occurred:', error);
-  });
-}
+    options.from = /"Stage": "Test"/g;
+    options.to = '"Stage": "${stage}"';
+    core.notice(`options: ${options}`);
+    try {
+      replaceInFiles(options)
+        .then(({ changedFiles, countOfMatchesByPaths }) => {
+          core.notice('Modified files:', changedFiles);
+          core.setOutput("changedFiles", changedFiles)
+          core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
+          core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
+          core.notice('options', options);
+          core.setOutput("options", options)
+        })
+        .catch(error => {
+          core.notice('Error occurred:', error);
+        });
+    }
 
 run()

fcadf29998afaa8e60b347223f7c0be26ca1893c
redasys <3372862+redasys@users.noreply.github.com>
2023-03-22 11:33:25 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+const replaceInFiles = require('replace-in-files');
+
+const options = {
+    files: ()=>{
+      return core.getInput('path-to-watch');
+    },
+    from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g,
+    to: '${ApiEndpoint}'
+}
+
+function run(){
+    replaceInFiles(options)
+  .then(({ changedFiles, countOfMatchesByPaths }) => {
+    core.notice('Modified files:', changedFiles);
+    core.setOutput("changedFiles", changedFiles)
+    core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
+    core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
+    core.notice('options', options);
+    core.setOutput("options", options)
+    
+  })
+  .catch(error => {
+    core.notice('Error occurred:', error);
+  });
+
+  options.from = /"Stage": "Test"/g;
+  options.to = '"Stage": "${stage}"';
+
+  replaceInFiles(options)
+  .then(({ changedFiles, countOfMatchesByPaths }) => {
+    core.notice('Modified files:', changedFiles);
+    core.setOutput("changedFiles", changedFiles)
+    core.notice('countOfMatchesByPaths', countOfMatchesByPaths);
+    core.setOutput("countOfMatchesByPaths", countOfMatchesByPaths)
+    core.notice('options', options);
+    core.setOutput("options", options)
+  })
+  .catch(error => {
+    core.notice('Error occurred:', error);
+  });
+}
+
+run()

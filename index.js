aae214a29df88526e508c8ff0c3719c61d8c6aa9
redasys <bob.egan@alliedpayment.com>
2023-03-22 20:02:15 -0400

    asf

--- index.js
+++ index.js
@@ -3,34 +3,35 @@ const replaceInFiles = require('replace-in-files');
 
 const options = {
   files: '',
-  from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g,
-  to: '${ApiEndpoint}'
+  // from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g,
+  // to: '${ApiEndpoint}'
 }
 
 function run() {
   try {
-    options.files = core.getInput('path-to-watch');
-    options.from = from: /jggveuruoh.execute-api.us-east-1.amazonaws.com/g
-    core.notice(options.files);
+    options.files = core.getInput('path-to-watch')
+    options.from = core.getInput('from')
+    options.to = core.getInput('to')
+    core.notice(JSON.stringify(options));
     replaceInFiles(options)
       .then(({ changedFiles, countOfMatchesByPaths }) => {
-        core.notice(`Modified files:, ${JSON.stringify(changedFiles)}`);
+        core.notice(`Modified files:, ${JSON.stringify(changedFiles)}`)
         core.setOutput(`changedFiles, ${JSON.stringify(changedFiles)}`)
-        core.notice(`countOfMatchesByPaths, ${JSON.stringify(countOfMatchesByPaths)}`);
+        core.notice(`countOfMatchesByPaths, ${JSON.stringify(countOfMatchesByPaths)}`)
         core.setOutput(`countOfMatchesByPaths, ${JSON.stringify(countOfMatchesByPaths)}`)
-        core.notice(`options, ${JSON.stringify(options)}`);
-        core.setOutput("options", options);
+        core.notice(`options, ${JSON.stringify(options)}`)
+        core.setOutput("options", options)
 
       })
     // .catch(error => {
-    //   core.notice(`Error occurred: ${error}${error.toString()}`);
+    //   core.notice(`Error occurred: ${error}${error.toString()}`)
     // });
   } catch {
-    core.notice(`Error occurred: ${error}${JSON.stringify(error)}`);
+    core.notice(`Error occurred: ${error}${JSON.stringify(error)}`)
   }
-  options.from = '"Stage": "Test"';
-  options.to = '"Stage": "${stage}"';
-  core.notice(`options: ${options}`);
+  options.from = "\"Stage\": \"Test\""
+  options.to = "\"Stage\": \"${stage}\""
+  core.notice(`options: ${options}`)
   try {
     replaceInFiles(options)
       .then(({ changedFiles, countOfMatchesByPaths }) => {
